d72038ea5d59a6156907ca2f9b91c95bf29a777f
giwoong.kim <kgw1988@naver.com>
2015-11-01 12:11:27 +0900

    [giwoong.kim][NOBTS] more PizzaFactory

---
+++ src/org/headfirst/model/VeggiePizza.java
@@ -0,0 +1,34 @@
+package org.headfirst.model;
+
+public class VeggiePizza implements Pizza {
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void bake() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cut() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void box() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Override
+	public String toString() {
+		return "VeggiePizza";
+	}
+
+}
